793477a7ddc6a84e4f1dd6f36bc0005239bbcccb
Kristian Perez <kpere192@fiu.edu>
2022-03-09 14:59:15 -0500

    Project 0 Banking Console

---
+++ BankingAPPConsole_KristianPerez/Accounts.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Accounts 
+{
+#region Account Properties
+    public int accNo { get; set; }
+    public string accName { get; set; }
+    public string accType { get; set; }
+    public double accBalance { get; set; }
+    public bool accIsActive { get; set; }
+    public string accEmail { get; set; }
+#endregion
+#region Account Methods
+#region Withdrawl
+public double Withdrawl(double amount)
+{
+    accBalance = accBalance - amount;
+    return accBalance;   
+}
+#endregion
+#region Deposit
+public double Deposit(double amount)
+{
+    accBalance = accBalance + amount;
+    return accBalance;   
+}
+#endregion
+#region GetAccountDetails
+public void GetAccountDetails()
+{
+    System.Console.WriteLine("--------------------------------");
+    System.Console.WriteLine("Account No: " + accNo);
+    System.Console.WriteLine("Name: " + accName);
+    System.Console.WriteLine("Type: " + accType);
+    System.Console.WriteLine("Balance: " + accBalance);
+    System.Console.WriteLine("Email: " + accEmail);
+    System.Console.WriteLine("--------------------------------");
+}
+#endregion
+#region CheckBalance
+public double CheckBalance()
+{
+    return accBalance;
+}
+#endregion
+#endregion
+}
---
+++ BankingAPPConsole_KristianPerez/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace oops_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isBanking = true;
+            int choice;
+            Accounts newAcc = new Accounts();
+
+            while(isBanking)
+            {
+                System.Console.WriteLine("1. Create new account");
+                System.Console.WriteLine("2. Check Balance");
+                System.Console.WriteLine("3. Withdrawl");
+                System.Console.WriteLine("4. Deposit");
+                System.Console.WriteLine("5. Get Details");
+                System.Console.WriteLine("6. Exit");
+
+                choice = Convert.ToInt32(System.Console.ReadLine());
+
+                switch(choice)
+                {
+                    #region Create New Account
+                    case 1:                        
+                        System.Console.WriteLine("Enter Account Number");
+                        newAcc.accNo = Convert.ToInt32(Console.ReadLine());    
+                        System.Console.WriteLine("Enter Account Name");
+                        newAcc.accName = System.Console.ReadLine();  
+                        System.Console.WriteLine("Enter Account Balance");
+                        newAcc.accBalance = Convert.ToDouble(Console.ReadLine());    
+                        System.Console.WriteLine("Enter Account Type");
+                        newAcc.accType = System.Console.ReadLine();    
+                        System.Console.WriteLine("Enter Account Email");
+                        newAcc.accEmail = System.Console.ReadLine();      
+                    break;
+                    #endregion
+                    #region Check Balance
+                    case 2:
+                        System.Console.WriteLine(newAcc.CheckBalance());
+                    break;
+                    #endregion
+                    #region Withdrawl
+                    case 3:
+                        System.Console.WriteLine("Enter amount to withdrawl");
+                        newAcc.Withdrawl(Convert.ToDouble(Console.ReadLine()));
+                        System.Console.WriteLine("Current Balance: $" + newAcc.accBalance);
+                    break;
+                    #endregion
+                    #region Deposit
+                    case 4:
+                        System.Console.WriteLine("Enter amount to deposit");
+                        newAcc.Deposit(Convert.ToDouble(System.Console.ReadLine()));
+                        System.Console.WriteLine("Current Balance: $" + newAcc.accBalance);
+                    break;
+                    #endregion
+                    case 5:
+                        newAcc.GetAccountDetails();
+                    break;
+                    case 6:
+                        isBanking = false;
+                    break;
+                    default:
+                        System.Console.WriteLine("Sorry " + choice + " is an invalid option");
+                    break;
+                }
+            }
+        }
+    }
+}
